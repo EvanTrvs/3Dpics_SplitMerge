076ef6ba9ace14a823b16059de98fe6da3d5fc9d
Thibaud SCRIBE <thibaud.scribe@etu.univ-tours.fr>
2023-11-12 10:28:29 +0100

    Correction Erreur taille = 1

--- GitHub_SplitAndMerge/CSplitOperation.cpp
+++ GitHub_SplitAndMerge/CSplitOperation.cpp
@@ -13,6 +13,7 @@
 vector <CFragment> CSplitOperation::SOTSplitMatrice(CFragment FRGMatrice, unsigned int CritereHomogeneite, vector <CFragment> vFRGVectorFinal) {
 
 	if (!FRGMatrice.FRGHomogeneity(CritereHomogeneite)) {
+
 		unsigned int uiDimX = static_cast<unsigned int> (round((double)FRGMatrice.FRGGetDimensions()[0] / 2)); //Coos X milieu
 		unsigned int uiDimY = static_cast<unsigned int> (round((double)FRGMatrice.FRGGetDimensions()[1] / 2)); //Coos Y milieu
 		unsigned int uiDimZ = static_cast<unsigned int> (round((double)FRGMatrice.FRGGetDimensions()[2] / 2)); //Coos Z milieu
@@ -24,26 +25,40 @@ vector <CFragment> CSplitOperation::SOTSplitMatrice(CFragment FRGMatrice, unsign
 		CFragment FRGNewFragmentNWF(FRGMatrice.FRGGetMatrice(), FRGMatrice.FRGGetCoos(), { uiDimX , uiDimY, uiDimZ }); //Fragment positionner au Nord + West + Front 
 		vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentNWF, CritereHomogeneite, vFRGVectorFinal); //Recursivité
 
-		CFragment FRGNewFragmentNWB(FRGMatrice.FRGGetMatrice(), { FRGMatrice.FRGGetCoos()[0], FRGMatrice.FRGGetCoos()[1], FRGMatrice.FRGGetCoos()[2] + uiDimZ }, { uiDimX , uiDimY, uiDimZ2 }); //Fragment positionner au Nord + West + Behind 
-		vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentNWB, CritereHomogeneite, vFRGVectorFinal); //Recursivité
+		if ((FRGMatrice.FRGGetDimensions()[2] != 1)) {
+			CFragment FRGNewFragmentNWB(FRGMatrice.FRGGetMatrice(), { FRGMatrice.FRGGetCoos()[0], FRGMatrice.FRGGetCoos()[1], FRGMatrice.FRGGetCoos()[2] + uiDimZ }, { uiDimX , uiDimY, uiDimZ2 }); //Fragment positionner au Nord + West + Behind 
+			vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentNWB, CritereHomogeneite, vFRGVectorFinal); //Recursivité
+		}
 
-		CFragment FRGNewFragmentNEF(FRGMatrice.FRGGetMatrice(), { FRGMatrice.FRGGetCoos()[0] + uiDimX, FRGMatrice.FRGGetCoos()[1], FRGMatrice.FRGGetCoos()[2] }, { uiDimX2 , uiDimY, uiDimZ }); //Fragment positionner au Nord + East + Front 
-		vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentNEF, CritereHomogeneite, vFRGVectorFinal); //Recursivité
+		if ((FRGMatrice.FRGGetDimensions()[0] != 1)) {
+			CFragment FRGNewFragmentNEF(FRGMatrice.FRGGetMatrice(), { FRGMatrice.FRGGetCoos()[0] + uiDimX, FRGMatrice.FRGGetCoos()[1], FRGMatrice.FRGGetCoos()[2] }, { uiDimX2 , uiDimY, uiDimZ }); //Fragment positionner au Nord + East + Front 
+			vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentNEF, CritereHomogeneite, vFRGVectorFinal); //Recursivité
+		}
 
-		CFragment FRGNewFragmentNEB(FRGMatrice.FRGGetMatrice(), { FRGMatrice.FRGGetCoos()[0] + uiDimX, FRGMatrice.FRGGetCoos()[1], FRGMatrice.FRGGetCoos()[2] + uiDimZ }, { uiDimX2 , uiDimY, uiDimZ2 }); //Fragment positionner au Nord + East + Behind 
-		vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentNEB, CritereHomogeneite, vFRGVectorFinal); //Recursivité
+		if ((FRGMatrice.FRGGetDimensions()[0] != 1) && (FRGMatrice.FRGGetDimensions()[2] != 1)) {
+			CFragment FRGNewFragmentNEB(FRGMatrice.FRGGetMatrice(), { FRGMatrice.FRGGetCoos()[0] + uiDimX, FRGMatrice.FRGGetCoos()[1], FRGMatrice.FRGGetCoos()[2] + uiDimZ }, { uiDimX2 , uiDimY, uiDimZ2 }); //Fragment positionner au Nord + East + Behind 
+			vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentNEB, CritereHomogeneite, vFRGVectorFinal); //Recursivité
+		}
 
-		CFragment FRGNewFragmentSWF(FRGMatrice.FRGGetMatrice(), { FRGMatrice.FRGGetCoos()[0], FRGMatrice.FRGGetCoos()[1] + uiDimY, FRGMatrice.FRGGetCoos()[2] }, { uiDimX , uiDimY2, uiDimZ }); //Fragment positionner au South + West + Front 
-		vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentSWF, CritereHomogeneite, vFRGVectorFinal); //Recursivité
+		if ((FRGMatrice.FRGGetDimensions()[1] != 1)) {
+			CFragment FRGNewFragmentSWF(FRGMatrice.FRGGetMatrice(), { FRGMatrice.FRGGetCoos()[0], FRGMatrice.FRGGetCoos()[1] + uiDimY, FRGMatrice.FRGGetCoos()[2] }, { uiDimX , uiDimY2, uiDimZ }); //Fragment positionner au South + West + Front 
+			vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentSWF, CritereHomogeneite, vFRGVectorFinal); //Recursivité
+		}
 
-		CFragment FRGNewFragmentSWB(FRGMatrice.FRGGetMatrice(), { FRGMatrice.FRGGetCoos()[0], FRGMatrice.FRGGetCoos()[1] + uiDimY, FRGMatrice.FRGGetCoos()[2] + uiDimZ }, { uiDimX , uiDimY2, uiDimZ2 }); //Fragment positionner au South + West + Behind 
-		vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentSWB, CritereHomogeneite, vFRGVectorFinal); //Recursivité
+		if ((FRGMatrice.FRGGetDimensions()[1] != 1) && (FRGMatrice.FRGGetDimensions()[2] != 1)) {
+			CFragment FRGNewFragmentSWB(FRGMatrice.FRGGetMatrice(), { FRGMatrice.FRGGetCoos()[0], FRGMatrice.FRGGetCoos()[1] + uiDimY, FRGMatrice.FRGGetCoos()[2] + uiDimZ }, { uiDimX , uiDimY2, uiDimZ2 }); //Fragment positionner au South + West + Behind 
+			vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentSWB, CritereHomogeneite, vFRGVectorFinal); //Recursivité
+		}
 
-		CFragment FRGNewFragmentSEF(FRGMatrice.FRGGetMatrice(), { FRGMatrice.FRGGetCoos()[0] + uiDimX, FRGMatrice.FRGGetCoos()[1] + uiDimY, FRGMatrice.FRGGetCoos()[2] }, { uiDimX2 , uiDimY2, uiDimZ }); //Fragment positionner au South + East + Front 
-		vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentSEF, CritereHomogeneite, vFRGVectorFinal); //Recursivité
+		if ((FRGMatrice.FRGGetDimensions()[0] != 1) && (FRGMatrice.FRGGetDimensions()[1] != 1)) {
+			CFragment FRGNewFragmentSEF(FRGMatrice.FRGGetMatrice(), { FRGMatrice.FRGGetCoos()[0] + uiDimX, FRGMatrice.FRGGetCoos()[1] + uiDimY, FRGMatrice.FRGGetCoos()[2] }, { uiDimX2 , uiDimY2, uiDimZ }); //Fragment positionner au South + East + Front 
+			vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentSEF, CritereHomogeneite, vFRGVectorFinal); //Recursivité
+		}
 
-		CFragment FRGNewFragmentSEB(FRGMatrice.FRGGetMatrice(), { FRGMatrice.FRGGetCoos()[0] + uiDimX, FRGMatrice.FRGGetCoos()[1] + uiDimY, FRGMatrice.FRGGetCoos()[2] + uiDimZ }, { uiDimX2 , uiDimY2, uiDimZ2 }); //Fragment positionner au South + East + Behind 
-		vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentSEB, CritereHomogeneite, vFRGVectorFinal); //Recursivité
+		if ((FRGMatrice.FRGGetDimensions()[0] != 1) && (FRGMatrice.FRGGetDimensions()[1] != 1) && (FRGMatrice.FRGGetDimensions()[2] != 1)) {
+			CFragment FRGNewFragmentSEB(FRGMatrice.FRGGetMatrice(), { FRGMatrice.FRGGetCoos()[0] + uiDimX, FRGMatrice.FRGGetCoos()[1] + uiDimY, FRGMatrice.FRGGetCoos()[2] + uiDimZ }, { uiDimX2 , uiDimY2, uiDimZ2 }); //Fragment positionner au South + East + Behind 
+			vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentSEB, CritereHomogeneite, vFRGVectorFinal); //Recursivité
+		}
 	}
 	else {
 		vFRGVectorFinal.push_back(FRGMatrice);
