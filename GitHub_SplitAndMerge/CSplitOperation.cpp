c7cb7514d618fa6b517901cbc59a65c262f723ba
Thibaud SCRIBE <thibaud.scribe@etu.univ-tours.fr>
2023-11-12 12:57:55 +0100

    Ajout Taille min Cube

--- GitHub_SplitAndMerge/CSplitOperation.cpp
+++ GitHub_SplitAndMerge/CSplitOperation.cpp
@@ -2,18 +2,20 @@
 
 #include "CSplitOperation.h"
 
-/************************************************************************************************************************************************
-***** SOTSPLITMATRICE : Méthode Divisant une matrice en fonction d'un critère d'homogénéité													*****
-*************************************************************************************************************************************************
-***** Entrée : FRGMatriceComplete : CFragment | CritereHomogeneite : unsigned int | vFRGVectorFinal : vector <CFragment>				    *****
-***** Nécessite : Ne nécessite rien                                                                                                         *****
-***** Sortie : vFRGLeafs : vector <CFragment>                                                                                               *****
-***** Entraine : Divise une matrice en fonction d'un critère d'homogénéité																	*****
-************************************************************************************************************************************************/
-vector <CFragment> CSplitOperation::SOTSplitMatrice(CFragment FRGMatrice, unsigned int CritereHomogeneite, vector <CFragment> vFRGVectorFinal) {
+/*****************************************************************************************************************************************************************
+***** SOTSPLITMATRICE : Méthode Divisant une matrice en fonction d'un critère d'homogénéité													                 *****
+******************************************************************************************************************************************************************
+***** Entrée : FRGMatriceComplete : CFragment | uiCritereHomogeneite : unsigned int | uiTailleMinCube : unsigned int | vFRGVectorFinal : vector <CFragment>	 *****
+***** Nécessite : Ne nécessite rien																															 *****
+***** Sortie : vFRGLeafs : vector <CFragment>																											     *****
+***** Entraine : Divise une matrice en fonction d'un critère d'homogénéité																					 *****
+*****************************************************************************************************************************************************************/
+vector <CFragment> CSplitOperation::SOTSplitMatrice(CFragment FRGMatrice, unsigned int uiCritereHomogeneite, unsigned int uiTailleMinCube, vector <CFragment> vFRGVectorFinal) {
 
-	if (!FRGMatrice.FRGHomogeneity(CritereHomogeneite)) {
+	unsigned int uiVolumeCube = FRGMatrice.FRGGetDimensions()[0] * FRGMatrice.FRGGetDimensions()[1] * FRGMatrice.FRGGetDimensions()[2];
+	if ((!FRGMatrice.FRGHomogeneity(uiCritereHomogeneite)) && (uiVolumeCube >= uiTailleMinCube * uiTailleMinCube*uiTailleMinCube)) {
 
+		cout << "taille : " << uiVolumeCube << endl;
 		unsigned int uiDimX = static_cast<unsigned int> (round((double)FRGMatrice.FRGGetDimensions()[0] / 2)); //Coos X milieu
 		unsigned int uiDimY = static_cast<unsigned int> (round((double)FRGMatrice.FRGGetDimensions()[1] / 2)); //Coos Y milieu
 		unsigned int uiDimZ = static_cast<unsigned int> (round((double)FRGMatrice.FRGGetDimensions()[2] / 2)); //Coos Z milieu
@@ -23,41 +25,41 @@ vector <CFragment> CSplitOperation::SOTSplitMatrice(CFragment FRGMatrice, unsign
 		unsigned int uiDimZ2 = FRGMatrice.FRGGetDimensions()[2] - uiDimZ; //Dim Pour Coos Z Milieu
 
 		CFragment FRGNewFragmentNWF(FRGMatrice.FRGGetMatrice(), FRGMatrice.FRGGetCoos(), { uiDimX , uiDimY, uiDimZ }); //Fragment positionner au Nord + West + Front 
-		vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentNWF, CritereHomogeneite, vFRGVectorFinal); //Recursivité
+		vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentNWF, uiCritereHomogeneite, uiTailleMinCube, vFRGVectorFinal); //Recursivité
 
 		if ((FRGMatrice.FRGGetDimensions()[2] != 1)) {
 			CFragment FRGNewFragmentNWB(FRGMatrice.FRGGetMatrice(), { FRGMatrice.FRGGetCoos()[0], FRGMatrice.FRGGetCoos()[1], FRGMatrice.FRGGetCoos()[2] + uiDimZ }, { uiDimX , uiDimY, uiDimZ2 }); //Fragment positionner au Nord + West + Behind 
-			vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentNWB, CritereHomogeneite, vFRGVectorFinal); //Recursivité
+			vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentNWB, uiCritereHomogeneite, uiTailleMinCube, vFRGVectorFinal); //Recursivité
 		}
 
 		if ((FRGMatrice.FRGGetDimensions()[0] != 1)) {
 			CFragment FRGNewFragmentNEF(FRGMatrice.FRGGetMatrice(), { FRGMatrice.FRGGetCoos()[0] + uiDimX, FRGMatrice.FRGGetCoos()[1], FRGMatrice.FRGGetCoos()[2] }, { uiDimX2 , uiDimY, uiDimZ }); //Fragment positionner au Nord + East + Front 
-			vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentNEF, CritereHomogeneite, vFRGVectorFinal); //Recursivité
+			vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentNEF, uiCritereHomogeneite, uiTailleMinCube, vFRGVectorFinal); //Recursivité
 		}
 
 		if ((FRGMatrice.FRGGetDimensions()[0] != 1) && (FRGMatrice.FRGGetDimensions()[2] != 1)) {
 			CFragment FRGNewFragmentNEB(FRGMatrice.FRGGetMatrice(), { FRGMatrice.FRGGetCoos()[0] + uiDimX, FRGMatrice.FRGGetCoos()[1], FRGMatrice.FRGGetCoos()[2] + uiDimZ }, { uiDimX2 , uiDimY, uiDimZ2 }); //Fragment positionner au Nord + East + Behind 
-			vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentNEB, CritereHomogeneite, vFRGVectorFinal); //Recursivité
+			vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentNEB, uiCritereHomogeneite, uiTailleMinCube, vFRGVectorFinal); //Recursivité
 		}
 
 		if ((FRGMatrice.FRGGetDimensions()[1] != 1)) {
 			CFragment FRGNewFragmentSWF(FRGMatrice.FRGGetMatrice(), { FRGMatrice.FRGGetCoos()[0], FRGMatrice.FRGGetCoos()[1] + uiDimY, FRGMatrice.FRGGetCoos()[2] }, { uiDimX , uiDimY2, uiDimZ }); //Fragment positionner au South + West + Front 
-			vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentSWF, CritereHomogeneite, vFRGVectorFinal); //Recursivité
+			vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentSWF, uiCritereHomogeneite, uiTailleMinCube, vFRGVectorFinal); //Recursivité
 		}
 
 		if ((FRGMatrice.FRGGetDimensions()[1] != 1) && (FRGMatrice.FRGGetDimensions()[2] != 1)) {
 			CFragment FRGNewFragmentSWB(FRGMatrice.FRGGetMatrice(), { FRGMatrice.FRGGetCoos()[0], FRGMatrice.FRGGetCoos()[1] + uiDimY, FRGMatrice.FRGGetCoos()[2] + uiDimZ }, { uiDimX , uiDimY2, uiDimZ2 }); //Fragment positionner au South + West + Behind 
-			vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentSWB, CritereHomogeneite, vFRGVectorFinal); //Recursivité
+			vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentSWB, uiCritereHomogeneite, uiTailleMinCube, vFRGVectorFinal); //Recursivité
 		}
 
 		if ((FRGMatrice.FRGGetDimensions()[0] != 1) && (FRGMatrice.FRGGetDimensions()[1] != 1)) {
 			CFragment FRGNewFragmentSEF(FRGMatrice.FRGGetMatrice(), { FRGMatrice.FRGGetCoos()[0] + uiDimX, FRGMatrice.FRGGetCoos()[1] + uiDimY, FRGMatrice.FRGGetCoos()[2] }, { uiDimX2 , uiDimY2, uiDimZ }); //Fragment positionner au South + East + Front 
-			vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentSEF, CritereHomogeneite, vFRGVectorFinal); //Recursivité
+			vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentSEF, uiCritereHomogeneite, uiTailleMinCube, vFRGVectorFinal); //Recursivité
 		}
 
 		if ((FRGMatrice.FRGGetDimensions()[0] != 1) && (FRGMatrice.FRGGetDimensions()[1] != 1) && (FRGMatrice.FRGGetDimensions()[2] != 1)) {
 			CFragment FRGNewFragmentSEB(FRGMatrice.FRGGetMatrice(), { FRGMatrice.FRGGetCoos()[0] + uiDimX, FRGMatrice.FRGGetCoos()[1] + uiDimY, FRGMatrice.FRGGetCoos()[2] + uiDimZ }, { uiDimX2 , uiDimY2, uiDimZ2 }); //Fragment positionner au South + East + Behind 
-			vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentSEB, CritereHomogeneite, vFRGVectorFinal); //Recursivité
+			vFRGVectorFinal = SOTSplitMatrice(FRGNewFragmentSEB, uiCritereHomogeneite, uiTailleMinCube, vFRGVectorFinal); //Recursivité
 		}
 	}
 	else {
--- GitHub_SplitAndMerge/CSplitOperation.h
+++ GitHub_SplitAndMerge/CSplitOperation.h
@@ -6,11 +6,15 @@
 
 class CSplitOperation {
 public:
-	vector <CFragment> SOTSplitMatrice(CFragment FRGMatriceComplete, unsigned int CritereHomogeneite, vector <CFragment> vFRGVectorFinal);
-
+	/*****************************************************************************************************************************************************************
+	***** SOTSPLITMATRICE : Méthode Divisant une matrice en fonction d'un critère d'homogénéité													                 *****
+	******************************************************************************************************************************************************************
+	***** Entrée : FRGMatriceComplete : CFragment | uiCritereHomogeneite : unsigned int | uiTailleMinCube : unsigned int | vFRGVectorFinal : vector <CFragment>	 *****
+	***** Nécessite : Ne nécessite rien																															 *****
+	***** Sortie : vFRGLeafs : vector <CFragment>																											     *****
+	***** Entraine : Divise une matrice en fonction d'un critère d'homogénéité																					 *****
+	*****************************************************************************************************************************************************************/
+	vector <CFragment> SOTSplitMatrice(CFragment FRGMatriceComplete, unsigned int CritereHomogeneite, unsigned int uiTailleMinCube, vector <CFragment> vFRGVectorFinal);
 };
-
-
-
 
 #endif
